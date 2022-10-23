cfb71f17d46dc7ba6417fe9248a0be235863bec2
Anas Abd Elazim <anas.ss@yahoo.com>
2022-10-24 00:43:59 +0200

    home process

--- lib/business logic/appCubit/appcubit_cubit.dart
+++ lib/business logic/appCubit/appcubit_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:herag/features/ad_details/actions/get_home_action.dart';
 
 part 'appcubit_state.dart';
 
@@ -6,4 +7,22 @@ class AppCubit extends Cubit<AppStates> {
   AppCubit() : super(AppInitial());
 
   static AppCubit get(context) => BlocProvider.of(context);
+  getHome(){
+    HomeAction action= HomeAction();
+    action.onSuccess=(res){};
+    action.onError=(res){};
+  }
+  getPosts(){
+    HomeAction action= HomeAction();
+    action.onSuccess=(res){};
+    action.onError=(res){};
+  }
+  getFavourites(){
+
+  }
+  contactUs(){
+
+  }
+  search(){}
+
 }
